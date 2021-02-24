e3a25737fdf63024421055ae6b41a0371cd0db7e
bartoszdziedzic <bartoszdziedzic91@gmail.com>
2021-02-24 19:07:14 +0100

    ActionsAndGestures

--- src/test/java/NavigationAndClosing/ActionsAndGestures.java
+++ src/test/java/NavigationAndClosing/ActionsAndGestures.java
@@ -3,9 +3,14 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class ActionsAndGestures {
@@ -40,5 +45,25 @@ public class ActionsAndGestures {
     public void contextClickExample(){
         driver.get("https://swisnl.github.io/jQuery-contextMenu/demo.html");
         actions.moveByOffset(417,208).contextClick().build().perform();
+    }
+
+    @Test
+    public void sendKeysExample(){
+        driver.get("https://fakestore.testelka.pl/moje-konto/");
+        driver.findElement(By.cssSelector(".woocommerce-store-notice__dismiss-link")).click();
+
+        WebElement login = driver.findElement(By.cssSelector("#username"));
+        actions.sendKeys(login,"qwe-1176").build().perform();
+        // actions.sendKeys(login, Keys.SHIFT, "qwe-1176").build().perform(); WPISZE Z SHIFTEM
+    }
+
+    @Test
+    public void keyUpDownExample(){
+        driver.get("https://jqueryui.com/selectable/#default");
+
+        driver.switchTo().frame(0);
+        List<WebElement> items = driver.findElements(By.cssSelector(".ui-selectable>li"));
+
+        actions.keyDown(Keys.CONTROL).click(items.get(0)).click(items.get(1)).click(items.get(2)).keyUp(Keys.CONTROL).build().perform();
     }
 }
